371dc7a408b0ef41c83cc4ad03f2199692a40ab9
MX6 <82836022+MX6-RBX@users.noreply.github.com>
2023-01-17 22:49:11 +0000

    Update DragLib.lua

--- DragLib.lua
+++ DragLib.lua
@@ -7,7 +7,7 @@ local DraggableObject 		= {}
 DraggableObject.__index 	= DraggableObject
 
 -- Sets up a new draggable object
-function DraggableObject:new(Object)
+function DraggableObject.new(Object)
 	local self 			= {}
 	self.Object			= Object
 	self.DragStarted	= nil
